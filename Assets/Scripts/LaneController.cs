0f15e9b65fddef8423e47d76bfa2daf467ae469b
yo-ch <yoson.chiu@hotmail.com>
2018-01-01 21:45:22 -0500

    Note lanes implemented.
    Note lanes.

---
+++ Assets/Scripts/LaneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneController : MonoBehaviour {
+
+    private Material mat;
+    private Color laneColor;
+    private Color pressedColor;
+    public string key;
+
+    // Use this for initialization
+    void Start () {
+        mat = GetComponent<Renderer>().material;
+        laneColor = mat.color;
+        pressedColor = new Color(0.3f, 0.3f, 0.3f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetKey(key)) {
+            mat.color = pressedColor;
+        } else {
+            mat.color = laneColor;
+        }
+    }
+}
